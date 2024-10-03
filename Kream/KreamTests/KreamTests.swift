66df218b40cc488106680e0e25cf7539e5dd821f
yun <rladbswls06@gmail.com>
2024-10-03 19:45:24 +0900

    2주차

---
+++ Kream/KreamTests/KreamTests.swift
@@ -0,0 +1,17 @@
+//
+//  KreamTests.swift
+//  KreamTests
+//
+//  Created by 김윤진 on 10/3/24.
+//
+
+import Testing
+@testable import Kream
+
+struct KreamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
