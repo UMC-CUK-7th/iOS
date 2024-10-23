360c30c788cb89ebcc49f85382957ad923b2d53d
yun-206 <rladbswls_06@naver.com>
2024-10-24 01:23:10 +0900

    3-4주차

--- iKREAM/iKREAM/Models/LoginModel.swift
+++ iKREAM/iKREAM/Models/LoginModel.swift
@@ -6,9 +6,60 @@
 //
 
 import Foundation
+import UIKit
 
 struct LoginModel {
     // id와 password를 저장하는 변수 선언
     var id: String
     var pwd: String
+}
+
+//3주차
+struct SavedModel {
+    let savedImage: String
+    let savedName: String
+    let savedDescription: String
+    let savedKeep : String
+    let savedPrice : String
+}
+
+//4주차
+struct HomeModel {
+    let homemodelImage: UIImage
+    let homemodelName: String
+}
+
+final class dummySavedModel {
+    static let savedDatas: [SavedModel] = [
+        SavedModel(savedImage: "son.png", savedName: "손오공이 잃어버린 머리띠 반쪽", savedDescription: "손오공이 장난치다가 머리띠 반쪽 잘라서 어딘가 숨겨둔 역사속 물건!!", savedKeep: "keep.png", savedPrice: "942,192,000원"),
+        SavedModel(savedImage: "goldring.png", savedName: "금색 반지", savedDescription: "결혼 준비하는 당신?! 약혼녀에게 이 반지를 선물하는 건 어때요?!", savedKeep: "keep.png", savedPrice: "12,000원"),
+        SavedModel(savedImage: "whiteshoes.png", savedName: "하얀 신발", savedDescription: "짝퉁 나이키 신발! 정말 푹신푹신해서 걷다가 졸려서 넘어지...", savedKeep: "keep.png", savedPrice: "90,000원"),
+        SavedModel(savedImage: "ring.png", savedName: "에베레스트 다이아 반지", savedDescription: "에베레스트 다이아 반지", savedKeep: "keep.png", savedPrice: "1,292,999,000원"),
+        SavedModel(savedImage: "adidas.png", savedName: "아디다스다 신발", savedDescription: "아디다스 처럼 보이지만 아디다스가 아닐지도 모르는 신발 이 신발의 주인은 너야~~", savedKeep: "keep.png", savedPrice: "120,000원"),
+        SavedModel(savedImage: "pajam.png", savedName: "황제 잠옷", savedDescription: "어느 나라의 황제가 입었다던 잠옷 크리스마스 트리같이 보이는 디자인!", savedKeep: "keep.png", savedPrice: "120,000,000원"),
+        SavedModel(savedImage: "guitar.png", savedName: "고장난 기타", savedDescription: "블라블라블라블라 블라블라블라블라블라", savedKeep: "keep.png", savedPrice: "120,000원"),
+        SavedModel(savedImage: "timber.png", savedName: "팀버랜드", savedDescription: "진짜사고싶은데너무비싸", savedKeep: "keep.png", savedPrice: "240,000원"),
+        SavedModel(savedImage: "padding.png", savedName: "검정 패딩", savedDescription: "추울 땐 패딩이지", savedKeep: "keep.png", savedPrice: "210,000원"),
+        SavedModel(savedImage: "force.png", savedName: "된장 포스", savedDescription: "된장포스를 아느냐", savedKeep: "keep.png", savedPrice: "140,000원"),
+    ]
+}
+
+extension HomeModel {
+    static func dummy() -> [HomeModel] {
+        return [
+            HomeModel(homemodelImage: .draw, homemodelName: "크림 드로우"),
+            HomeModel(homemodelImage: .chart, homemodelName: "실시간 차트"),
+            HomeModel(homemodelImage: .mans, homemodelName: "남성 추천"),
+            HomeModel(homemodelImage: .womans, homemodelName: "여성 추천"),
+            HomeModel(homemodelImage: .special, homemodelName: "색다른 추천"),
+            HomeModel(homemodelImage: .low, homemodelName: "정가 아래"),
+            HomeModel(homemodelImage: .yunsea, homemodelName: "윤세24FW"),
+            HomeModel(homemodelImage: .best, homemodelName: "올해의 베스트"),
+            HomeModel(homemodelImage: .benefit, homemodelName: "10월 베네핏"),
+            HomeModel(homemodelImage: .acene, homemodelName: "아크네 선물"),
+
+        ]
+        
+    }
+
 }
--- iKREAM/iKREAM/ViewControllers/ManageProfileViewController.swift
+++ iKREAM/iKREAM/ViewControllers/ManageProfileViewController.swift
@@ -11,76 +11,97 @@ class ManageProfileViewController: UIViewController {
     // MARK: - Properties
     let manageProfileView = ManageProfileView()
     
+    // 데이터를 받을 public 변수 선언
+    public var receivedData: UIImage?
     
+
     // MARK: - Life Cycle
     override func loadView() {
         self.view = manageProfileView
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        setNavigationController() // viewDidLoad에 적는것도 가능하긴 하다!
+
+        setNavigationController()
+        loadUserData() // UserDefaults에서 데이터 로드
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         setButtonActions()
+        // 전달받은 데이터를 이미지 뷰에 표시
+        if let data = receivedData {
+            self.manageProfileView.profileImageView.image = data
+        }
+        
     }
-    
-    // 화면의 빈곳을 터치한 경우에 키보드를 내리는 코드
+
+    // 화면의 빈 곳을 터치한 경우에 키보드를 내리는 코드
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
-    
+
+    // MARK: - Load User Data
+    private func loadUserData() {
+        // UserDefaults에서 이메일과 비밀번호를 로드
+        let savedEmail = UserDefaults.standard.string(forKey: "userEmail") ?? ""
+        let savedPassword = UserDefaults.standard.string(forKey: "userPassword") ?? ""
+        manageProfileView.emailTextField.text = savedEmail
+        manageProfileView.passwordTextField.text = savedPassword
+    }
+
     // MARK: - Set Button Actions
     private func setButtonActions() {
-        // 버튼 action관리
         manageProfileView.emailUpdateButton.addTarget(self, action: #selector(didTapEmailUpdateButton), for: .touchUpInside)
         manageProfileView.passwordUpdateButton.addTarget(self, action: #selector(didTapPasswordUpdateButton), for: .touchUpInside)
-        
     }
-    
+
     // MARK: - Set NavigationController
     private func setNavigationController() {
         self.title = "프로필 관리"
         self.navigationController?.navigationBar.tintColor = .black
-        let backButtonImage = UIImage(named: "BackImage") //뒤로 가기
+        let backButtonImage = UIImage(named: "BackImage")
         self.navigationController?.navigationBar.backIndicatorImage = backButtonImage
         self.navigationController?.navigationBar.backIndicatorTransitionMaskImage = backButtonImage
         self.navigationController?.navigationBar.topItem?.backButtonTitle = ""
     }
-    
+
     // MARK: - Selectors
+    
     @objc
     private func didTapEmailUpdateButton() {
         if !manageProfileView.emailTextField.isEnabled {
             manageProfileView.emailUpdateButton.setTitle("확인", for: .normal)
             manageProfileView.emailTextField.isEnabled = true
-            manageProfileView.emailTextField.becomeFirstResponder() // 이메일 textField 자동 선택
-             
+            manageProfileView.emailTextField.becomeFirstResponder()
         } else {
+            // "확인" 버튼 클릭 시 이메일 저장
+            let updatedEmail = manageProfileView.emailTextField.text ?? ""
+            UserDefaults.standard.set(updatedEmail, forKey: "userEmail")
+
             manageProfileView.emailUpdateButton.setTitle("변경", for: .normal)
             manageProfileView.emailTextField.isEnabled = false
+    
         }
     }
-    
-    // 비밀번호 변경 버튼 눌리면 실행할 함수
+
     @objc
     private func didTapPasswordUpdateButton() {
-        // (실제로 이렇게 개발하진 않는다는 코멘트)
         if !manageProfileView.passwordTextField.isEnabled {
             manageProfileView.passwordUpdateButton.setTitle("확인", for: .normal)
             manageProfileView.passwordTextField.isEnabled = true
             manageProfileView.passwordTextField.isSecureTextEntry = false
-            manageProfileView.passwordTextField.becomeFirstResponder() // 비밀번호 textField 자동 선택
-            
+            manageProfileView.passwordTextField.becomeFirstResponder()
         } else {
+            // "확인" 버튼 클릭 시 비밀번호 저장
+            let updatedPassword = manageProfileView.passwordTextField.text ?? ""
+            UserDefaults.standard.set(updatedPassword, forKey: "userPassword")
+
             manageProfileView.passwordUpdateButton.setTitle("변경", for: .normal)
             manageProfileView.passwordTextField.isEnabled = false
             manageProfileView.passwordTextField.isSecureTextEntry = true
-            
         }
     }
 }
