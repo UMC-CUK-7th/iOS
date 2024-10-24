c55ed63067e02dd46023cadc289b27356bb79388
hanbi <hanbi07070506@gmail.com>
2024-10-24 16:15:57 +0900

    3,4주차 과제

--- Kream/Kream/Models/LoginModel.swift
+++ Kream/Kream/Models/LoginModel.swift
@@ -7,8 +7,31 @@
 
 import Foundation
 
-struct LoginUser {
-    var id: String
-    var pwd: String
+/*struct LoginUser {
+ var id: String
+ var pwd: String
+ 
+ }*/
+
+class LoginModel {
+    private let userIpnut = UserDefaults.standard
+    // private let loginPWD = UserDefaults.standard
+    //private let userTextKey: String = "loginID"
     
+    // 유저가 입력한 텍스트 값을 UserDefaults에 저장.
+    public func saveUserID(_ text: String) {
+        userIpnut.set(text, forKey: "loginID")
+    }
+    public func saveUserPWD(_ text: String) {
+        userIpnut.set(text, forKey: "loginPWD")
+    }
+    
+    // UserDefaults에 저장된 값을 불러옴
+    public func loadUserID() -> String? {
+        return userIpnut.string(forKey: "loginID")
+    }
+    public func loadUserPWD() -> String? {
+        return userIpnut.string(forKey: "loginPWD")
+    }
 }
+
--- Kream/Kream/ViewControllers/SearchViewController.swift
+++ Kream/Kream/ViewControllers/SearchViewController.swift
@@ -8,22 +8,15 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    let searchView = SearchView()
+    
+    override func loadView() {
+        self.view = searchView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
