0ff44849923eb810d38c2f4191a4bcc3be2a7621
yun-206 <rladbswls_06@naver.com>
2024-11-06 18:05:13 +0900

    OPP

--- iKREAM/iKREAM/ViewControllers/SearchViewController.swift
+++ iKREAM/iKREAM/ViewControllers/SearchViewController.swift
@@ -5,17 +5,24 @@
 //  Created by 김윤진 on 10/30/24.
 //
 
+//
+//  SearchViewController.swift
+//  iKREAM
+//
+//  Created by 김윤진 on 10/30/24.
+//
+
 import UIKit
 import SnapKit
 
 class SearchViewController: UIViewController, UICollectionViewDelegateFlowLayout {
     private let searchView = SearchView()
-    
+
     // MARK: - Life Cycle
     override func loadView() {
         self.view = searchView
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
@@ -23,47 +30,52 @@ class SearchViewController: UIViewController, UICollectionViewDelegateFlowLayout
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // 뒤로가기 버튼 커스텀
+        configureNavigationBar()
+        configureButtons()
+        setupDelegate()
+    }
+
+    // 네비게이션 바 설정
+    private func configureNavigationBar() {
         let backButtonImage = UIImage(named: "backImage 1")?.withRenderingMode(.alwaysOriginal)
         let resizedBackButtonImage = resizeImage(image: backButtonImage, targetSize: CGSize(width: 24, height: 24))
-        
-        let backButton = UIBarButtonItem(image: resizedBackButtonImage, style: .done, target: self, action: #selector(backButtonTapped))
-        backButton.tintColor = .black // 아이콘 색상 설정
 
-        // 현재 뷰 컨트롤러의 네비게이션 아이템에 커스텀 버튼 설정
+        let backButton = UIBarButtonItem(image: resizedBackButtonImage, style: .done, target: self, action: #selector(backButtonTapped))
+        backButton.tintColor = .black
         navigationItem.leftBarButtonItem = backButton
-        // 구매 버튼 액션 추가
-        searchView.buyButton.addTarget(self, action: #selector(didTapBuyButton), for: .touchUpInside)
-        setupDelegate()
-    
     }
-    
+
+    // 버튼 설정
+    private func configureButtons() {
+        searchView.buyButton.addTarget(self, action: #selector(didTapBuyButton), for: .touchUpInside)
+    }
+
     private func setupDelegate() {
         searchView.matinCollectionView.dataSource = self
         searchView.matinCollectionView.delegate = self
     }
-    
-    // 뒤로가기 버튼을 눌렀을 때 동작
+
+    // 뒤로가기 버튼 액션
     @objc private func backButtonTapped() {
         navigationController?.popViewController(animated: true)
     }
-    
-    // 구매 버튼 클릭 시 호출되는 함수
+
+    // 구매 버튼 액션
     @objc private func didTapBuyButton() {
-        let modalVC = SellViewController() // 모달로 표시할 뷰 컨트롤러
-        modalVC.modalPresentationStyle = .pageSheet // 시트 방식으로 표시
-        present(modalVC, animated: true, completion: nil) // 모달 표시
+        let modalVC = SellViewController()
+        modalVC.modalPresentationStyle = .pageSheet
+        present(modalVC, animated: true, completion: nil)
     }
-    
-    // 이미지를 특정 크기로 리사이즈하는 함수
+
+    // 이미지 리사이즈
     private func resizeImage(image: UIImage?, targetSize: CGSize) -> UIImage? {
         guard let image = image else { return nil }
-        
+
         let size = image.size
-        let widthRatio  = targetSize.width  / size.width
+        let widthRatio = targetSize.width / size.width
         let heightRatio = targetSize.height / size.height
         let newSize = CGSize(width: size.width * widthRatio, height: size.height * heightRatio)
-        
+
         UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
         image.draw(in: CGRect(origin: .zero, size: newSize))
         let newImage = UIGraphicsGetImageFromCurrentImageContext()
@@ -76,55 +88,39 @@ class SearchViewController: UIViewController, UICollectionViewDelegateFlowLayout
 // MARK: - UICollectionViewDataSource
 extension SearchViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // 올바른 CollectionView 참조
-        if collectionView == searchView.matinCollectionView {
-            return SearchModel.dummy().count
-        }
-        return 0
+        return SearchModel.dummy().count
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        // 올바른 CollectionView 참조
-        if collectionView == searchView.matinCollectionView {
-            guard let cell = collectionView.dequeueReusableCell(
-                withReuseIdentifier: matinCollectionViewCell.identifier,
-                for: indexPath
-            ) as? matinCollectionViewCell else {
-                return UICollectionViewCell()
-            }
-            let model = SearchModel.dummy()[indexPath.row] // SearchModel로 데이터 모델을 수정
-            cell.configure(with: model)
-            return cell
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: matinCollectionViewCell.identifier,
+            for: indexPath
+        ) as? matinCollectionViewCell else {
+            return UICollectionViewCell()
         }
-        return UICollectionViewCell()
+
+        let model = SearchModel.dummy()[indexPath.row]
+        cell.configure(with: model)
+        return cell
     }
 }
 
 // MARK: - UICollectionViewDelegateFlowLayout
 extension SearchViewController {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        if collectionView == searchView.matinCollectionView {
-            return CGSize(width: 53, height: 53)
-        }
-        return CGSize.zero
+        return CGSize(width: 53, height: 53)
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        if collectionView == searchView.matinCollectionView {
-            return 8
-        }
-        return 0
+        return 8
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        if collectionView == searchView.matinCollectionView {
-            return 9
-        }
-        return 0
+        return 9
     }
 }
 
-// MARK: - collectionView : Configure func.ver
+// MARK: - matinCollectionViewCell Extension
 extension matinCollectionViewCell {
     func configure(with model: SearchModel) {
         matinView.image = model.searchImage
