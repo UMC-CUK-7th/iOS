90591276ada9301b4408fe409cb97aea2e86fd08
hanbi <hanbi07070506@gmail.com>
2024-11-13 16:45:59 +0900

    [Add] Keychain 구현
    토큰, 닉네임 Keychain에  저장

--- Kream/Kream/Models/KakaoAuthViewModel.swift
+++ Kream/Kream/Models/KakaoAuthViewModel.swift
@@ -13,6 +13,8 @@ import KakaoSDKUser
 class KakaoAuthViewModel: ObservableObject {
     var subscriptions = Set<AnyCancellable>()
     
+    
+    //MARK: - KakaoLogin
     @Published // 로그인 여부
     var isLogined: Bool = false
     
@@ -28,8 +30,10 @@ class KakaoAuthViewModel: ObservableObject {
                 else {
                     print("loginWithKakaoTalk() success.")
                     
-                    // 로그인 성공 시 동작 구현
-                    _ = oauthToken
+                    // 로그인 성공 시 토큰 저장
+                    if let token = oauthToken {
+                        self.storeTokenAndAccount(oauthToken: token)
+                    }
                     continuation.resume(returning: true)
                 }
             }
@@ -48,8 +52,10 @@ class KakaoAuthViewModel: ObservableObject {
                 else {
                     print("loginWithKakaoAccount() success.")
                     
-                    // 로그인 성공 시 동작 구현
-                    _ = oauthToken
+                    // 로그인 성공 시 토큰 저장
+                    if let token = oauthToken {
+                        self.storeTokenAndAccount(oauthToken: token)
+                    }
                     continuation.resume(returning: true)
                 }
             }
@@ -72,4 +78,34 @@ class KakaoAuthViewModel: ObservableObject {
             }
         }
     }
+    
+    //MARK: - Keychain
+    // 토큰과 닉네임을 Keychain에 저장하는 메서드
+    func storeTokenAndAccount(oauthToken: OAuthToken) {
+        let token = oauthToken.accessToken
+        
+        UserApi.shared.me { user, error in
+            if let error = error {
+                print("Error fetching user info: \(error)")
+            }
+            else if let nickname = user?.kakaoAccount?.profile?.nickname {
+                // 앱의 고유 식별자를 Keychain의 서비스 이름으로 사용
+                let service = Bundle.main.bundleIdentifier ?? "KakaoLoginService"
+                
+                // 토큰과 닉네임을 Keychain에 저장
+                let tokenSaveStatus = KeychainService.shared.save(token: token, account: "accessToken", service: service)
+                let nicknameSaveStatus = KeychainService.shared.save(token: nickname, account: "userNickname", service: service)
+                
+                // 저장 확인용
+                let nickname = KeychainService.shared.load(account: "userNickname", service: service) // Keychain에 저장된 닉네임을 확인하기 위해 불러옴
+                if tokenSaveStatus == errSecSuccess && nicknameSaveStatus == errSecSuccess {
+                    print("토큰과 닉네임이 성공적으로 Keychain에 저장되었습니다.")
+                    print("저장된 닉네임 : \(nickname)")
+                } else {
+                    print("Keychain 저장에 실패했습니다. 상태 코드: \(tokenSaveStatus), \(nicknameSaveStatus)")
+                }
+            }
+        }
+    }
+    
 }
